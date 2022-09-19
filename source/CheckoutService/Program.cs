cd9db6fe7380aa356cfb4ea561ea000a6ab09921
Phuoc <Phuoc.Quach@positivethinking.tech>
2022-09-19 09:22:15 +0700

    update port

--- source/CheckoutService/Program.cs
+++ source/CheckoutService/Program.cs
@@ -17,12 +17,8 @@ namespace CheckoutService
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
